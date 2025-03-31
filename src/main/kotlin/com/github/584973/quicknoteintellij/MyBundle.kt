8ef29f4c4bbf1cb13bbe21ac1964bec80c02d739
GitHub Action <action@github.com>
2025-03-31 18:50:02 +0000

    Template cleanup

--- src/main/kotlin/com/github/584973/quicknoteintellij/MyBundle.kt
+++ src/main/kotlin/com/github/584973/quicknoteintellij/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.584973.quicknoteintellij
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/584973/quicknoteintellij/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/584973/quicknoteintellij/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.584973.quicknoteintellij.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
